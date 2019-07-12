f5084911028eaaf35aa08107a03ced467d692ecc
QuintyHH <razmocanu@yahoo.com>
2019-07-12 04:20:02 +0300

    Wrapped up the app.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -5,6 +5,7 @@ const KEY ="AIzaSyDlBWXtsd103H7qo81KZusu_u9yRMKE7iM"
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
+    type: 'video',
     part: 'snippet',
     maxResults: 5,
     key: KEY
