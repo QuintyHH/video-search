b3a972f3e49261d999de0e2455bb0d87bf6e64f6
QuintyHH <razmocanu@yahoo.com>
2019-07-12 04:27:29 +0300

    Updates

---
+++ precache-manifest.95bc2e477041ce89da6f719049c16043.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650c12e06bd1857ae9c2a05e41506d85",
+    "url": "/video-search/index.html"
+  },
+  {
+    "revision": "e43b74b9a5a43b2979b7",
+    "url": "/video-search/static/css/main.ca161dc0.chunk.css"
+  },
+  {
+    "revision": "e9c2ec1ac571ad625f91",
+    "url": "/video-search/static/js/2.60637844.chunk.js"
+  },
+  {
+    "revision": "e43b74b9a5a43b2979b7",
+    "url": "/video-search/static/js/main.857fd216.chunk.js"
+  },
+  {
+    "revision": "74833dc51c18c4085545",
+    "url": "/video-search/static/js/runtime~main.301bb524.js"
+  }
+]);
